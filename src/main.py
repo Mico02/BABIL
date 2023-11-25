e3d4d4a945729e39aaa8efa60f239b9691688017
Mico02 <mohamed.hussein122702@gmail.com>
2023-11-25 16:57:34 -0500

    programmed close-to realtime speech recognition

---
+++ src/main.py
@@ -0,0 +1,46 @@
+import sounddevice
+import sys
+import queue
+import json
+import argostranslate.package
+import argostranslate.translate
+from vosk import Model, KaldiRecognizer
+q=queue.Queue()
+def callback(indata, frames, time, status):
+    """This is called (from a separate thread) for each audio block."""
+    if status:
+        print(status, file=sys.stderr)
+    q.put(bytes(indata))
+
+
+#Using command line arguments for determining what languages to use
+from_code = sys.argv[1]
+to_code = sys.argv[2]
+
+#Getting microphone info
+device = None
+audio_input = sounddevice.query_devices(device, kind="input")
+sample_rate = audio_input["default_samplerate"]
+print(device)
+
+#Constants for audio data capture
+BLOCK_SIZE = 8000
+CHANNELS = 1
+
+#Initalizing vosk model
+model = Model(lang=from_code)
+
+#Initalizing vosk recognizer
+recognizer = KaldiRecognizer(model, sample_rate)
+
+#print(audio_input)
+with sounddevice.RawInputStream(samplerate=sample_rate, blocksize=BLOCK_SIZE,dtype="int16",callback=callback,channels=1):
+     while True:
+        data = q.get()
+        if recognizer.AcceptWaveform(data):
+            final = recognizer.FinalResult()
+            print(final)
+        else: 
+            partial_result = json.loads(recognizer.PartialResult())
+            if len(partial_result.get("partial")) > 0:
+                print(partial_result)
