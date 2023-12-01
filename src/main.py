494d4ee22e0334fa4151d6854b42d259949e1e05
Mico02 <mohamed.hussein122702@gmail.com>
2023-11-30 19:38:33 -0500

    made cmd line args for lang codes universal

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from vosk import Model, KaldiRecognizer
 from display import OLEDDisplay
 
 
-
+#setting up call back for continous audio capture
 q=queue.Queue()
 def callback(indata, frames, time, status):
     """This is called (from a separate thread) for each audio block."""
@@ -21,6 +21,28 @@ def callback(indata, frames, time, status):
 from_code = sys.argv[1]
 to_code = sys.argv[2]
 
+
+#dictionary that stores the lang codes (used for lang codes that don't match between vosk and argos)
+#vosk lang codes as the key, and argos/libre lang codes as the value 
+
+vosk_langs = {"en", "es", "fr", "ar", "cn", "pt", "ar", "fa", "hi"}
+translate_langs = {"en", "es", "fr", "ar", "zh", "pt", "ar", "fa", "hi"}
+
+#vosk lang codes as the key
+vosk_lang_map = {
+    "en" : "en", 
+    "es" : "es", 
+    "fr" : "fr", 
+    "ar" : "ar", 
+    "cn" : "zh", 
+    "pt" : "pt", 
+    "ar" : "ar", 
+    "fa" : "fa", 
+    "hi" : "hi", 
+}
+
+argos_lang_map = {value: key for key, value in vosk_lang_map.items()}
+
 #Getting microphone info
 device = None
 audio_input = sounddevice.query_devices(device, kind="input")
@@ -31,7 +53,8 @@ BLOCK_SIZE = 10000
 CHANNELS = 1
 
 #Initalizing vosk model
-model = Model(lang=from_code)
+lang = from_code if from_code in vosk_langs else argos_lang_map.get(from_code)
+model = Model(lang=lang)
 
 #Initalizing vosk recognizer
 recognizer = KaldiRecognizer(model, sample_rate)
@@ -41,10 +64,8 @@ display = OLEDDisplay(font_size = 15)
 
 #Initializing translator API
 translator = LibreTranslateAPI("https://translate.terraprint.co/")
-#translatergt = Translator()
 
 prev_idx = 0
-#print(audio_input)
 with sounddevice.RawInputStream(samplerate=sample_rate, blocksize=BLOCK_SIZE,dtype="int16",callback=callback,channels=CHANNELS):
      print("############## START ############## ")
      display.displayWord("*** START ***") 
@@ -57,7 +78,9 @@ with sounddevice.RawInputStream(samplerate=sample_rate, blocksize=BLOCK_SIZE,dty
 
             if len(words) > 0 :        
                 print("**** ENTERED TRANSLATION *** ")
-                translated = translator.translate(q=words, source=from_code, target=to_code)
+                src = from_code if from_code in translate_langs else vosk_lang_map.get(from_code)
+                dst = to_code if to_code in translate_langs else vosk_lang_map.get(to_code)
+                translated = translator.translate(q=words, source=src, target=dst)
                 print(translated)
                 display.displayWords(translated.split(" "))
                 
