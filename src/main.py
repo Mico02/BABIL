e5691a1724784bce02449ab52feb7bf975f14316
Mico02 <mohamed.hussein122702@gmail.com>
2024-01-26 21:03:29 -0500

    made display object a parameter the transcriber constructor

--- src/main.py
+++ src/main.py
@@ -1,19 +1,23 @@
 import sys
 from transcription import Transcriber
+from display import OLEDDisplay
 
-def transcribe(language):
-    transcriber = Transcriber(language=language)
+def transcribe(language, display):
+    transcriber = Transcriber(language=language, display=display)
     transcriber.run()
     return
 
 def translate(from_code, to_code):
     return
 
+#Initializing display object
+display = OLEDDisplay(font_size=15)
+
 #Read cmd arguements 
 mode = sys.argv[1]
 if mode == '-c':
     language = sys.argv[2]
-    transcribe(language=language)
+    transcribe(language,display)
     exit()
 else: 
     from_code = sys.argv[1]
--- src/transcription.py
+++ src/transcription.py
@@ -6,13 +6,14 @@ import json
 from vosk import Model, KaldiRecognizer
 
 class Transcriber:
-    def __init__(self, language, audio_block_size=10000, audio_channels=1, audio_data_type="int16"):
+    def __init__(self, language, display, audio_block_size=10000, audio_channels=1, audio_data_type="int16"):
         #Specifing audio capture data
         #self.sample_rate = sample_rate
         self.__BLOCK_SIZE = audio_block_size
         self.__CHANNELS = audio_channels
         self.__dtype = audio_data_type
         self.__queue = queue.Queue()
+        self.__display = display
 
         #Getting microphone information
         device = None
@@ -23,21 +24,22 @@ class Transcriber:
         self.recognizer = KaldiRecognizer(self.model, self.sample_rate)
 
     #setting up call back for continous audio capture
-    def __callback(self,self, indata, frames, time, status):
+    def __callback(self, indata, frames, time, status):
         """This is called (from a separate thread) for each audio block."""
         if status:
             print(status, file=sys.stderr)
         self.__queue.put(bytes(indata))
 
     def run(self):
-        with sounddevice.RawInputStream(samplerate=self.sample_rate, blocksize=self.BLOCK_SIZE,dtype=self.dtype,callback=self.__callback,channels=self.CHANNELS):
+        with sounddevice.RawInputStream(samplerate=self.sample_rate, blocksize=self.__BLOCK_SIZE,dtype=self.__dtype,callback=self.__callback,channels=self.__CHANNELS):
             print("############## START ############## ")
-            self.display.displayWord("*** START ***") 
+            self.__display.displayWord("*** START ***") 
             while True:
-                data = self.queue.get()
+                data = self.__queue.get()
                 if self.recognizer.AcceptWaveform(data):
-                    words = json.loads(self.recognizer.FinalResult()).get("text")
+                    words = json.loads(self.recognizer.FinalResult()).get("text").split(" ")
                     print(words)
+                    self.__display.displayWords(words)
                 else: 
                     partial_result = json.loads(self.recognizer.PartialResult())
-                    print(partial_result.get("partial"))
+                    #print(partial_result.get("partial"))
