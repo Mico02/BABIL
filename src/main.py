25066fd384d15cdbea2c9adcde6664beda33ca0d
Mico02 <mohamed.hussein122702@gmail.com>
2024-02-20 13:31:02 -0500

    added a button listener for the main program

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import sys
+import threading
 from transcription import Transcriber
 from display import OLEDDisplay
+from button import ButtonHandler, PowerButtonHandler
 
 def transcribe(language, display):
     transcriber = Transcriber(language=language, display=display)
@@ -11,6 +13,14 @@ def translate(from_code, to_code):
     return
 
 def main():
+
+    #initializing buttonhandler and power/reboot thread
+    button = ButtonHandler()
+    powerbutton = PowerButtonHandler()
+    reboot = threading.Thread(target=button.reboot_reselect)
+    power = threading.Thread(target=powerbutton.wait_for_press)
+    reboot.start()
+    power.start()
     #Initializing display object
     display = OLEDDisplay(font_size=15)
 
@@ -19,13 +29,16 @@ def main():
     if mode == '-c':
         language = sys.argv[2]
         transcribe(language,display)
-        exit()
+        sys.exit()
     else: 
         from_code = sys.argv[1]
         to_code = sys.argv[2]
         if from_code == to_code:
             transcribe(language=from_code)
-            exit()
+            sys.exit()
         translate(from_code=from_code, to_code=to_code)
-        exit()
+        sys.exit()
+    reboot.join()
+    power.join()
+
 main()
