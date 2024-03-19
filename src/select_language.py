7f0f44e592cd2a3a12ccffd9dea29662b34a9792
Mico02 <mohamed.hussein122702@gmail.com>
2024-03-18 22:35:35 -0400

    set up UI for translation

--- src/select_language.py
+++ src/select_language.py
@@ -13,7 +13,7 @@ caption_languages = {"en" : "English",
                      "pt" : "Portuguese", 
                      "gr" : "Greek", 
                      "tr" : "Turkish", 
-                     "vn" : "Vietnamese ", 
+                     "vn" : "Vietnamese", 
                      "it" : "Italian", 
                      "nl" : "Dutch",
                      "ar" : "Arabic",
@@ -32,10 +32,78 @@ caption_languages = {"en" : "English",
                      "ko" : "Korean",
                      "br" : "Breton",
                      "gu" : "Gujarati"} 
+from_translation_languages = {"en" : "English",
+                            "cn" : "Chinese", 
+                            "ru" : "Russian", 
+                            "fr" : "French", 
+                            "de" : "German", 
+                            "es" : "Spanish", 
+                            "pt" : "Portuguese", 
+                            "gr" : "Greek", 
+                            "tr" : "Turkish", 
+                            "vn" : "Vietnamese", 
+                            "it" : "Italian", 
+                            "nl" : "Dutch",
+                            "ar" : "Arabic",
+                            "ca" : "Catalan",
+                            "fa" : "Farsi",
+                            "ph" : "Filipino",
+                            "uk" : "Ukrainian",
+                            "sv" : "Swedish",
+                            "jp" : "Japanese",
+                            "eo" : "Esparanto",
+                            "hi" : "Hindi",
+                            "cs" : "Czech",
+                            "po" : "Polish",
+                            "uz" : "Uzbek",
+                            "ko" : "Korean"}
 
-from_lang_translation = {
-    
-}
+to_translation_languages = {"en" : "English",
+                            "sq" : "Albanian",
+                            "ar" : "Arabic",
+                            "az" : "Azerbaijani",
+                            "bn" : "Bengali",
+                            "bg" : "Bulgarian",
+                            "ca" : "Catalan",
+                            "zh" : "Chinese",
+                            "zt" : "Chinese (traditional)",
+                            "cs" : "Czech",
+                            "da" : "Danish",
+                            "nl" : "Dutch",
+                            "eo" : "Esperanto",
+                            "et" : "Estonian",
+                            "fi" : "Finnish",
+                            "fr" : "French",
+                            "de" : "German",
+                            "el" : "Greek",
+                            "he" : "Hebrew",
+                            "hi" : "Hindi",
+                            "hu" : "Hungarian",
+                            "id" : "Indonesian",
+                            "ga" : "Irish",
+                            "it" : "Italian",
+                            "ja" : "Japanese",
+                            "ko" : "Korean",
+                            "lv" : "Latvian",
+                            "lt" : "Lithuanian",
+                            "ms" : "Malay",
+                            "nb" : "Norwegian",
+                            "fa" : "Farsi",
+                            "pl" : "Polish",
+                            "pt" : "Portuguese",
+                            "ro" : "Romanian",
+                            "ru" : "Russian",
+                            "sr" : "Serbian",
+                            "sk" : "Slovak",
+                            "sl" : "Slovenian",
+                            "es" : "Spanish",
+                            "sv" : "Swedish",
+                            "tl" : "Filipino",
+                            "th" : "Thai",
+                            "tr" : "Turkish",
+                            "uk" : "Ukrainian",
+                            "ur" : "Urdu",
+                            "vi" : "Vietnamese"}
     
 
 def select_device_mode(buttons: ButtonHandler, display: OLEDDisplay):
@@ -93,10 +161,14 @@ if mode == "Captioning":
     print("GOODBYE")
     exit()
 elif mode == "Translation":
-    from_lang = select_language(buttons, from_lang_translation, display)
-    print(f"{caption_languages.get(from_lang)} is the selected from lang")
-    
-    to_lang = select_language(buttons, to_lang_translation, display)
-    print(f"{caption_languages.get(from_lang)} is the selected to lang")
+    from_lang = select_language(buttons, from_translation_languages, display)
+    print(f"{from_translation_languages.get(from_lang)} is the selected from lang")
+    to_lang = select_language(buttons, to_translation_languages, display)
+    print(f"{to_translation_languages.get(to_lang)} is the selected to lang")
+    powerButton.stop_waiting()
+    powerButtonThread.join()
+    subprocess.Popen(["python3" ,"src/main.py",from_lang, to_lang])
+    print("GOODBYE")
+    exit()
 
 
