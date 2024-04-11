2a68b8e563881b25fa553aed81a75f57d0f59085
Mico02 <mohamed.hussein122702@gmail.com>
2024-04-11 13:24:10 -0400

    added testing options

--- src/main.py
+++ src/main.py
@@ -10,10 +10,20 @@ def transcribe(language, display):
     transcriber.run()
     return
 
+def transcribeTest(language, display):
+    transcriber = Transcriber(language=language, display=display)
+    transcriber.runTranscriptionTest()
+    return
+
 def translate(from_code, to_code, display):
     transcriber = Transcriber(language=from_code, display=display)
     translatator = Translator(from_code=from_code, to_code=to_code)
     transcriber.runWithTranslation(translatator,5)
+    return
+def translateTest(from_code, to_code, display):
+    transcriber = Transcriber(language=from_code, display=display)
+    translatator = Translator(from_code=from_code, to_code=to_code)
+    transcriber.runWithTranslationTest(translatator,5)
     return
 
 def main():
@@ -33,6 +43,17 @@ def main():
     if mode == '-c':
         language = sys.argv[2]
         transcribe(language,display)
+        sys.exit()
+    elif mode == '-tc': 
+        language = sys.argv[2]
+        transcribeTest(language,display)
+        sys.exit()
+    elif mode == '-tt':
+        from_code = sys.argv[2]
+        to_code = sys.argv[3]
+        phrase = sys.argv[4]
+        translator = Translator(from_code,to_code)
+        translator.runTest(phrase)
         sys.exit()
     else: 
         from_code = sys.argv[1]
